f984913f4607a4e8acb03536916ecf7b87f20d8c
habibarezq <habibarezq30@gmail.com>
2024-11-10 12:43:02 +0200

    Added a Function

--- VectorDrawingApplication/src/Interfaces/Shape.java
+++ VectorDrawingApplication/src/Interfaces/Shape.java
@@ -17,8 +17,10 @@ public interface Shape {
     public void setColor(Color color);
     public Color getColor();
     public void setFillColor(Color color);
+    public Color getFillColor();
     //Redraw the shape on the canvas
     public void draw(java.awt.Graphics canvas0);
     
     public String getName();
+    public void setShapeName(String shapeName);
 }
