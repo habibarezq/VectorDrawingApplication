365b1488879872ec7c905e0f17db67c0a8485220
habibarezq <habibarezq30@gmail.com>
2024-11-27 18:32:21 +0200

    Adjusted setPropreties

--- VectorDrawingApplication/src/Backend/Circle.java
+++ VectorDrawingApplication/src/Backend/Circle.java
@@ -11,13 +11,19 @@ public class Circle extends DefaultShape {
     public Circle(Point position,String shapeName,double radius) {
         super(position,shapeName);
         super.properties.put("radius", radius);
-        
+        super.properties.put("x2", (double)position.x);
+        super.properties.put("y2",(double)position.y);
     }
     
     @Override
     public void setProperties(Map<String, Double> properties) {
     if(properties.containsKey("radius"))
         this.properties.put("radius",properties.get("radius"));
+    if(properties.containsKey("x2"))
+        this.properties.put("x2",properties.get("x2"));
+    if(properties.containsKey("y2"))
+        this.properties.put("y2",properties.get("y2"));
+    
     }
 
     @Override
@@ -28,8 +34,12 @@ public class Circle extends DefaultShape {
     @Override
     public void draw(Graphics canvas) {
         double radius=this.properties.get("radius");
+        double x,y;
+        x=this.properties.get("x2");
+        y=this.properties.get("y2");
+        
         canvas.setColor(this.getColor());
-        canvas.drawOval((this.getPosition().x)-(int)radius,(this.getPosition().y)-(int)radius,2*(int)radius, 2*(int)radius);
+        canvas.drawOval((int)(x)-(int)radius,(int)(y)-(int)radius,2*(int)radius, 2*(int)radius);
         
     }
     
