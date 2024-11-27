24eb7acc317d6fadba285c916034beeeec2f647c
habibarezq <habibarezq30@gmail.com>
2024-11-27 18:32:28 +0200

    Adjusted setPropreties

--- VectorDrawingApplication/src/Backend/Rectangle.java
+++ VectorDrawingApplication/src/Backend/Rectangle.java
@@ -13,8 +13,8 @@ public class Rectangle extends DefaultShape {
         super(position,shapeName);
         super.setColor(Color.BLACK);
         super.setFillColor(Color.WHITE);
-        super.properties.put("x", (double)position.x);
-        super.properties.put("y",(double)position.y);
+        super.properties.put("x2", (double)position.x);
+        super.properties.put("y2",(double)position.y);
         super.properties.put("width",width);
         super.properties.put("height",height);
     }
@@ -25,10 +25,10 @@ public class Rectangle extends DefaultShape {
         this.properties.put("width",properties.get("width"));
     if(properties.containsKey("height"))
         this.properties.put("height",properties.get("height"));
-    if(properties.containsKey("x"))
-        this.properties.put("x",properties.get("x"));
-    if(properties.containsKey("y"))
-        this.properties.put("y",properties.get("y"));
+    if(properties.containsKey("x2"))
+        this.properties.put("x2",properties.get("x2"));
+    if(properties.containsKey("y2"))
+        this.properties.put("y2",properties.get("y2"));
     
     }
     
@@ -42,8 +42,12 @@ public class Rectangle extends DefaultShape {
     public void draw(Graphics canvas) {
         double width=this.properties.get("width");
         double height=this.properties.get("height");
+        double x,y;
+        x=this.properties.get("x2");
+        y=this.properties.get("y2");
+        
         canvas.setColor(this.getColor());
-        canvas.drawRect((this.getPosition().x)-(int)width,(this.getPosition().y)-(int)height,2*(int)width, 2*(int)height);
+        canvas.drawRect((int)x-(int)width,(int)y-(int)height,2*(int)width, 2*(int)height);
         
 //        canvas.fillRect((this.getPosition().x)-(int)width,(this.getPosition().y)-(int)height,2*(int)width, 2*(int)height);
     }
