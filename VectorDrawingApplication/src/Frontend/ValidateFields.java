e31a729731a3067c94552e67e7f0dc0cd89b262e
habibarezq <habibarezq30@gmail.com>
2024-11-27 18:31:11 +0200

    Adjusted some validations

--- VectorDrawingApplication/src/Frontend/ValidateFields.java
+++ VectorDrawingApplication/src/Frontend/ValidateFields.java
@@ -5,21 +5,21 @@ package Frontend;
 public class ValidateFields {
     public static boolean validateCoordinates(int x,int y)
     {
-        if(x<0 || x>400 || y>350 || y<0 )
+        if(x<0 || x>650 || y>350 || y<0 )
             return false;
         else
             return true;
     }
     public static boolean validateCoordinates(int x,int y,double length)
     {
-        if(x<0 || x>400 || y>350 || y<0 || length <0)
+        if(x<0 || x>650 || y>350 || y<0 || length <0)
             return false;
         else
             return true;
     }
     public static boolean validateCoordinates(int x,int y,double width,double length)
     {
-        if(x<0 || x>400 || y>350 || y<0 || width <0 || length <0 )
+        if(x<0 || x>650 || y>350 || y<0 || width <0 || length <0 )
             return false;
         else
             return true;
