9ad91b973a8016297c311993eb6598836036f804
habibarezq <habibarezq30@gmail.com>
2024-11-29 19:11:12 +0200

    Function added to drawingEngine

--- VectorDrawingApplication/src/Interfaces/DrawingEngine.java
+++ VectorDrawingApplication/src/Interfaces/DrawingEngine.java
@@ -1,12 +1,15 @@
 
 package Interfaces;
 
+import java.util.Map;
+
 
 public interface DrawingEngine {
     //Manage shapes Objects 
     public void addShape(Shape shape);
     public void removeShape(Shape shape);
-    
+    public void moveShape(Shape shape,Map<String, Double> properties);
+    public void resizeShape(Shape shape,Map<String, Double> properties);
     //return the created shapes objects
     public Shape[] getShapes();
     
