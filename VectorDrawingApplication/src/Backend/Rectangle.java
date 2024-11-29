678489b2df7de10dd21498573f9702797a66616b
habibarezq <habibarezq30@gmail.com>
2024-11-29 05:59:00 +0200

    Fixed Colorize

--- VectorDrawingApplication/src/Backend/Rectangle.java
+++ VectorDrawingApplication/src/Backend/Rectangle.java
@@ -9,22 +9,22 @@ import java.util.Map;
 
 public class Rectangle extends DefaultShape {
 
-    public Rectangle(Point position,String shapeName,double width,double height) {
+    public Rectangle(Point position,String shapeName,double width,double length) {
         super(position,shapeName);
         super.setColor(Color.BLACK);
         super.setFillColor(Color.WHITE);
         super.properties.put("x2", (double)position.x);
         super.properties.put("y2",(double)position.y);
         super.properties.put("width",width);
-        super.properties.put("height",height);
+        super.properties.put("length",length);
     }
     
     @Override
     public void setProperties(Map<String, Double> properties) {
     if(properties.containsKey("width"))
         this.properties.put("width",properties.get("width"));
-    if(properties.containsKey("height"))
-        this.properties.put("height",properties.get("height"));
+    if(properties.containsKey("length"))
+        this.properties.put("length",properties.get("length"));
     if(properties.containsKey("x2"))
         this.properties.put("x2",properties.get("x2"));
     if(properties.containsKey("y2"))
@@ -41,15 +41,15 @@ public class Rectangle extends DefaultShape {
     @Override
     public void draw(Graphics canvas) {
         double width=this.properties.get("width");
-        double height=this.properties.get("height");
+        double length=this.properties.get("length");
         double x,y;
         x=this.properties.get("x2");
         y=this.properties.get("y2");
         
         canvas.setColor(this.getColor());
-        canvas.drawRect((int)x-(int)width,(int)y-(int)height,2*(int)width, 2*(int)height);
-        
-//        canvas.fillRect((this.getPosition().x)-(int)width,(this.getPosition().y)-(int)height,2*(int)width, 2*(int)height);
+        canvas.drawRect((int)x-(int)width,(int)y-(int)length,2*(int)width, 2*(int)length);
+        canvas.setColor(this.getFillColor());
+        canvas.fillRect((int)x-(int)width,(int)y-(int)length,2*(int)width, 2*(int)length);
     }
     
 }
